407797fca2f0884732d8c39703ae7c73f927e591
carolmbithe <carombithe97@gmail.com>
2018-10-12 15:03:02 +0300

    first migration

--- manage.py
+++ manage.py
@@ -1,12 +1,16 @@
 from app import create_app,db
 from flask_script import Manager,Server
 from app.models import User,Role
+from flask_migrate import Migrate,MigrateCommand
 
 
 #Creating app instance
 app = create_app('development')
 
+migrate = Migrate(app,db)
 manager = Manager(app)
+
+manager.add_command('db',MigrateCommand)
 manager.add_command('server',Server)
 
 @manager.shell
