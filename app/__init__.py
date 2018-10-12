a7e736a88ce9c4ce35d284b89c4334d573494d67
carolmbithe <carombithe97@gmail.com>
2018-10-12 21:59:58 +0300

    can upload an image

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 from config import config_options
 from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
+from flask_uploads import UploadSet,configure_uploads,IMAGES
+from flask_mail import Mail
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
@@ -11,11 +13,16 @@ login_manager.login_view = 'auth.login'
 
 bootstrap = Bootstrap()
 db=SQLAlchemy()
+photos = UploadSet('photos',IMAGES)
+mail = Mail()
 
 
 def create_app(config_name):
 
     app = Flask(__name__)
+
+
+    #configure UploadSet
 
 
     #Creating the app configurations
@@ -25,6 +32,7 @@ def create_app(config_name):
     bootstrap.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
+    mail.init_app(app)
 
     #Registering the blueprint
     from .main import main as main_blueprint
@@ -33,6 +41,7 @@ def create_app(config_name):
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
 
+    configure_uploads(app,photos)
 
 
     return app
--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
 export SECRET_KEY=4033
+export MAIL_USERNAME = mumocaroline19@gmail.com
+export MAIL_PASSWORD=@carol1234
 
 python3.6 manage.py server
